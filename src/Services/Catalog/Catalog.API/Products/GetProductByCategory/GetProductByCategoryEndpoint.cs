02d4f3eadc034ae07b6c7a0efa5bd06cc9fb9dc1
Ligesh <ligeshkoshy@gmail.com>
2025-03-12 12:03:10 +1100

    Updated Get Products by Category via Vertical slice architecture and tested via localhost to docker - postgres data

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+//Adding both Request and Response as record type for Minimal API-Carter
+
+//This is masked as parameter comes via request url
+//public record GetProductByCategoryRequest();
+
+//Response - Record type - API Return - list of products
+
+/// <summary>
+/// Implement Endpoint via Minimal API and Carter to fetch products by category name
+/// </summary>
+/// <param name="Product"></param>
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    //Implement route for GET Api
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        //Carter route handler and builder - Adding a category in path as we already have id api 
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            //Get result via Mediatr - Input Category value comes from request parameter
+            var result = await sender.Send(new GetProductByCategoryQuery(category));
+            //Using Mapster to perform object mapping with GetProductByCategoryResponse and GetProductByCategoryResult
+            var response = result.Adapt<GetProductByCategoryResponse>();
+            return Results.Ok(response);
+        })
+            .WithName("GetProductsByCategory")
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products By Category")
+            .WithDescription("Get Products By Category");
+    }
+}
+
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,27 +1,23 @@
-﻿
+﻿namespace Catalog.API.Products.GetProductById;
 
-using Catalog.API.Products.GetProducts;
-
-namespace Catalog.API.Products.GetProductById
+//public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+public class GetProductByIdEndpoint : ICarterModule
 {
-    //public record GetProductByIdRequest();
-    public record GetProductByIdResponse(Product Product);
-    public class GetProductByIdEndpoint : ICarterModule
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
-        public void AddRoutes(IEndpointRouteBuilder app)
+        app.MapGet("/products/{Id}", async (Guid id, ISender sender) =>
         {
-            app.MapGet("/products/{Id}",async(Guid id, ISender sender) =>
-            {
-                var result = await sender.Send(new GetProductByIdQuery(id));
-                var response = result.Adapt<GetProductByIdResponse>();
-                return Results.Ok(response);
-            })
-            .WithName("GetProductById")
-            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
-            .ProducesProblem(StatusCodes.Status400BadRequest)
-            .WithSummary("Get Products By Id")
-            .WithDescription("Get Products By Id");
-        }
-
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            var response = result.Adapt<GetProductByIdResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products By Id")
+        .WithDescription("Get Products By Id");
     }
+
 }
+
