bf47f0f26a764a0789ab3eac3c03573f92cd0b23
Ligesh <ligeshkoshy@gmail.com>
2025-03-19 10:53:59 +1100

    Implemented Redis caching (IDistributedCache) for Basket service with Postgres & Redis health checks, tested locally via Docker

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,12 @@
+using HealthChecks.UI.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config => {
+builder.Services.AddMediatR(config =>
+{
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
@@ -18,7 +21,25 @@ builder.Services.AddMarten(options =>
 
 //Add dependency injection for BasketRepository
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//Need to cache CachebasketRepository from IBasketRepository - But we cant have 2 registrations as the second with override the first
+//One many is manual injection via code. Better way is the use Scrutor library - which registers (decorates) already registered service
+
+//Install Scrutor via nuget and use Decorate method
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+//Register IDistributedCache
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+//Add dependency injection for ExceptionHandler
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+//Register healthchecks for Postgres DB and Redis cache
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
@@ -26,4 +47,12 @@ var app = builder.Build();
 app.MapCarter();
 //Include exception in request pipeline
 app.UseExceptionHandler(options => { });
+
+//Implement health checks - Install nuget health.UI.client - to get health checkwriter to return response as JSON format
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 app.Run();
