ed29b1a01952992068b8fcfd8137483f437d1cbb
Ligesh <ligeshkoshy@gmail.com>
2025-03-19 18:25:15 +1100

    Overwritten the Discount gRPC service, updated the request pipeline in Program.cs, and successfully tested the invocation via Postman.

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+/// <summary>
+/// Implement DiscountService inherting from DiscountProtoServiceBase, created by Visual studio when building the project
+/// To implement the Discount Service use case, we need to override the default DiscountProtoServiceBase methods
+/// </summary>
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    //When Grpc call comes in, we override the exisitng methods and execute the custom logic here
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        return base.GetDiscount(request, context);
+    }
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+}
