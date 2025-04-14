188f92bd0278d7dd371c4b8ed1f4d2523bec2d4e
Ligesh <ligeshkoshy@gmail.com>
2025-04-14 12:37:54 +1000

    Build common entity for all interfaces

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+/// <summary>
+/// Generic interface for all entities with unique identifier of type T to implement entity classes
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+/// <summary>
+/// IEntity interface with common properties
+/// </summary>
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
