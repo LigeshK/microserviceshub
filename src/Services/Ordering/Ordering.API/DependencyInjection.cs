38ceb7981b497a8d32c40f713c290d85e851871e
Ligesh <ligeshkoshy@gmail.com>
2025-04-14 10:10:54 +1000

    Refactor and build the Ordering Microservices project using Domain-Driven Design (DDD), CQRS, and Clean Architecture principles. Structure the solution into API, Application, Domain, and Infrastructure layers, ensuring proper dependency flow and alignment with architectural building blocks.

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Add API Extensions
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        //services.AddCarter();
+
+        return services;
+    }
+    /// <summary>
+    /// Use API extensions
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.MapCarter();
+        return app;
+    }
+
+}
